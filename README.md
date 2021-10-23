c49a25d622300430ee6f3b58172d4888fd9dd22b
tonis <tonisives@gmail.com>
2021-10-23 09:53:49 +0700

    add get strategy details method

---
+++ README.md
@@ -0,0 +1,12 @@
+# Iconomi Aggregator
+Aggregate top 10 Iconomi strategy balances.
+
+
+## Setup
+
+Add config.py with keys
+
+```
+ICONOMI_API_SECRET = ""
+ICONOMI_API_KEY = ""
+```
--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import hmac
+import hashlib
+import base64
+
 from chalice import Chalice
 
+from config import *
+import requests
+import time
+import json
+
 app = Chalice(app_name='iconomi-aggregator')
+ICONOMI_API_URL = "https://api.iconomi.com"
 
 
 @app.route('/')
@@ -8,7 +18,52 @@ def index():
     return {'hello': 'world'}
 
 
+@app.route('/detail/{ticker}')
+def detail(ticker):
+    assets = get("/v1/strategies/" + ticker + "/structure")["values"]
+    return assets
 
+def generate_signature(payload, request_type, request_path, timestamp):
+    data = ''.join([timestamp, request_type.upper(),
+                    request_path, payload]).encode()
+    signed_data = hmac.new(ICONOMI_API_SECRET.encode(), data, hashlib.sha512)
+    return base64.b64encode(signed_data.digest())
+
+
+def get(api):
+    return call('GET', api, '')
+
+
+def post(api, payload):
+    return call('POST', api, payload)
+
+
+def call(method, api, payload):
+    timestamp = str(int(time.time() * 1000.0))
+
+    jsonPayload = payload
+    if method == 'POST':
+        jsonPayload = json.dumps(payload)
+
+    requestHeaders = {
+        'ICN-API-KEY': ICONOMI_API_KEY,
+        'ICN-SIGN': generate_signature(jsonPayload, method, api, timestamp),
+        'ICN-TIMESTAMP': timestamp
+    }
+
+    if method == 'GET':
+        response = requests.get(ICONOMI_API_URL + api, headers=requestHeaders)
+        if response.status_code == 200:
+            return json.loads(response._content)
+        else:
+            return ('Request did not succeed: ' + response.reason)
+    elif method == 'POST':
+        response = requests.post(
+            ICONOMI_API_URL + api, json=payload, headers=requestHeaders)
+        if response.status_code == 200:
+            return json.loads(response._content)
+        else:
+            return ('Request did not succeed: ' + response.reason)
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
